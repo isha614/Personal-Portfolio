bcb060c3467d6fd0a1a90efa0b2eba8ffdf89603
isha614 <ishamehta614@gmail.com>
2021-08-01 00:00:34 +0530

    Menu button javascript and rotate

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,9 +8,18 @@ const newTextDelay = 2000; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
 
+
+const menuBtn=document.querySelector('.menu-btn');
+const menu=document.querySelector('.menu');
+const menuNav=document.querySelector('.menu-nav');
+const menuBranding=document.querySelector('.menu-branding');
+const navItems=document.querySelectorAll('.nav-item');
+
+
+
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.remove("typing");
     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
@@ -23,7 +32,7 @@ function type() {
 
 function erase() {
 	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.remove("typing");
     typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
     charIndex--;
     setTimeout(erase, erasingDelay);
@@ -39,3 +48,32 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
   if(textArray.length) setTimeout(type, newTextDelay + 250);
 });
+
+
+
+//Set initial state of Menu
+let showMenu=false;
+menuBtn.addEventListener('click',toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu)
+    {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        //Set Menu State
+        showMenu= true;
+    } else{
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //Set Menu State
+        showMenu= false;         
+    }
+}
